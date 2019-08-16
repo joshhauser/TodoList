4b35eb559b5b929adf842266d8b71f259dd0bf7c
joshhauser <none>
2019-08-16 14:04:05 +0200

    adjusted INSERT for task

--- PHP/API/tasks/DELETE_TASK.php
+++ PHP/API/tasks/DELETE_TASK.php
@@ -2,7 +2,6 @@
 require_once("../db_settings.php");
 
 $id = $_GET['id'];
-
 $table= "tasks";
 
 $connection = mysqli_connect($server,$login,$dbPass)
@@ -11,13 +10,10 @@ or die("Impossible to connect");
 mysqli_select_db($connection,$db)
 or die("Database not found.");
 
-// Delete board
 $deleteRequest = "DELETE FROM $table WHERE id='$id'";
 $result = mysqli_query($connection, $deleteRequest);
 
-// Delete tasks which are part of the board
-$deleteRequest = "DELETE FROM tasks WHERE boardID = $id";
-$result = mysqli_query($connection, $deleteRequest);
 
 mysqli_close($connection);
+
 ?>
